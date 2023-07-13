18f08ddcba65a7fac7236bf5d523c801fcb810fd
Antonio Luiz Mangueira <developer@mangueira.net>
2023-07-12 23:53:42 -0300

    feat: add search games by list

--- src/main/java/br/dev/manga/dslist/services/GameService.java
+++ src/main/java/br/dev/manga/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package br.dev.manga.dslist.services;
 import br.dev.manga.dslist.dto.GameDTO;
 import br.dev.manga.dslist.dto.GameMinDTO;
 import br.dev.manga.dslist.entities.Game;
+import br.dev.manga.dslist.projections.GameMinProjection;
 import br.dev.manga.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,12 @@ public class GameService {
   @Transactional(readOnly = true)
   public List<GameMinDTO> findAll() {
     List<Game> result = gameRepository.findAll();
+    return result.stream().map(GameMinDTO::new).toList();
+  }
+
+  @Transactional(readOnly = true)
+  public List<GameMinDTO> findByList(Long listid) {
+    List<GameMinProjection> result = gameRepository.searchByList(listid);
     return result.stream().map(GameMinDTO::new).toList();
   }
 }
